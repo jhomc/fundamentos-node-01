c7d4793b93b54b4f78f68d589b68f144084c17cc
jhonata costa <jhomcosta@gmail.com>
2023-01-16 08:20:17 -0300

    first commit

--- src/server.js
+++ src/server.js
@@ -1,7 +1,25 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
-  return res.end('hello world')
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res.setHeader('Content-type', 'application/json').end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: users.length + 1,
+      name: req.body.name,
+      email: req.body.email
+    })
+
+    return res.end('Usuário criado com sucesso')
+  }
+
+  return res.end('hello ignite')
 })
 
 server.listen(3333)
